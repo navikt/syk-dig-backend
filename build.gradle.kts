b5565ae4b09ddd73cf6c60afc2690ca716c9bb88
Tia Firing <tia.firing@nav.no>
2022-08-30 16:14:17 +0200

    databaseoppsett

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ import com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask
 
 plugins {
 	id("org.springframework.boot") version "2.7.0"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.21"
-	kotlin("plugin.spring") version "1.6.21"
+	id("io.spring.dependency-management") version "1.0.13.RELEASE"
+	kotlin("jvm") version "1.7.10"
+	kotlin("plugin.spring") version "1.7.10"
 	id("com.netflix.dgs.codegen") version "5.1.17"
 }
 
@@ -17,6 +17,8 @@ repositories {
 	mavenCentral()
 }
 
+val testContainersVersion = "1.17.3"
+
 dependencies {
 	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:latest.release"))
 	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter")
@@ -26,7 +28,11 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("io.micrometer:micrometer-registry-prometheus")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.postgresql:postgresql")
+	implementation("org.flywaydb:flyway-core")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.testcontainers:postgresql:$testContainersVersion")
 }
 
 tasks.withType<KotlinCompile> {
