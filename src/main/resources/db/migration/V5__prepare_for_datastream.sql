9d3f39a22e38babcb41b4b7388b5d635c2507500
Andreas Nilsen <danduras@gmail.com>
2023-02-01 14:43:52 +0100

    prepare for datastream

--- src/main/resources/db/migration/V5__prepare_for_datastream.sql
+++ src/main/resources/db/migration/V5__prepare_for_datastream.sql
@@ -1,8 +1,18 @@
-alter user "syk-dig-backend-db-instance" with replication;
-create publication syk_dig_publication for ALL TABLES;
-SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT
-           ('syk_dig_replication', 'pgoutput');
-alter user "syk-dig-datastream-user" with replication;
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO "datastream-syk-dig-user";
-GRANT USAGE ON SCHEMA public TO "datastream-syk-dig-user";
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO "datastream-syk-dig-user";
+DO
+$$
+    BEGIN
+        IF EXISTS
+            (SELECT 1 from pg_roles where rolname = 'syk-dig-backend-db-instance')
+        THEN
+            alter user "syk-dig-backend-db-instance" with replication;
+            create publication syk_dig_publication for ALL TABLES;
+            SELECT PG_CREATE_LOGICAL_REPLICATION_SLOT
+                       ('syk_dig_replication', 'pgoutput');
+            alter user "syk-dig-datastream-user" with replication;
+            GRANT SELECT ON ALL TABLES IN SCHEMA public TO "datastream-syk-dig-user";
+            GRANT USAGE ON SCHEMA public TO "datastream-syk-dig-user";
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO "datastream-syk-dig-user";
+
+        END IF;
+    END
+$$;
