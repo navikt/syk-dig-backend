2a0b5e2c7ce9e538115534c633f881c6b49e88b0
Karl J. Overå <k@rl.run>
2023-02-14 15:43:56 +0100

    expose multiple documents in GQL API

--- src/main/kotlin/no/nav/sykdig/digitalisering/api/DocumentController.kt
+++ src/main/kotlin/no/nav/sykdig/digitalisering/api/DocumentController.kt
@@ -7,30 +7,35 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.HttpServerErrorException
 
 @RestController
-class PdfController(
+class DocumentController(
     private val oppgaveRepository: OppgaveRepository,
     private val safClient: SafClient,
 ) {
     val log = logger()
 
-    @Deprecated("Bruk /api/document/{oppgaveId}/{dokumentInfoId} i stedet")
-    @GetMapping("/api/pdf", produces = [MediaType.APPLICATION_PDF_VALUE])
+    @GetMapping("/api/document/{oppgaveId}/{dokumentInfoId}", produces = [MediaType.APPLICATION_PDF_VALUE])
     @PreAuthorize("@oppgaveSecurityService.hasAccessToOppgave(#oppgaveId)")
     @ResponseBody
-    fun getPdf(@RequestParam oppgaveId: String): ByteArray {
+    fun getDocument(@PathVariable oppgaveId: String, @PathVariable dokumentInfoId: String): ByteArray {
         val oppgave = oppgaveRepository.getOppgave(oppgaveId)
+
         if (oppgave != null) {
+            if (oppgave.dokumenter?.any { it.dokumentInfoId == dokumentInfoId } == false || oppgave.dokumentInfoId != dokumentInfoId) {
+                log.error("$dokumentInfoId er ikke en del av oppgave $oppgaveId")
+                throw HttpClientErrorException(HttpStatus.BAD_REQUEST, "dokumentInfoId er ikke gyldig for oppgaven")
+            }
+
             try {
                 return safClient.hentPdfFraSaf(
+                    dokumentInfoId = if (dokumentInfoId == "primary") oppgave.dokumentInfoId else dokumentInfoId,
                     journalpostId = oppgave.journalpostId,
-                    dokumentInfoId = oppgave.dokumentInfoId ?: "",
                     sykmeldingId = oppgave.sykmeldingId.toString(),
                 )
             } catch (e: Exception) {
