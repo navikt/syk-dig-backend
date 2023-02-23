5f5c3466189f04a66e0e7cbce79c89b50782dada
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-02-23 12:43:32 +0100

    Override tomcatEmbedCoreVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ val jaxbVersion = "2.4.0-b180830.0438"
 val javaxActivationVersion = "1.1.1"
 val javaTimeAdapterVersion = "1.1.3"
 val graphqlDgsPlatformDependenciesVersion = "5.5.1"
+val tomcatEmbedCoreVersion = "9.0.71"
 
 dependencies {
     implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:$graphqlDgsPlatformDependenciesVersion"))
@@ -56,6 +57,7 @@ dependencies {
     implementation("com.netflix.graphql.dgs:graphql-dgs-extended-scalars")
     implementation("com.graphql-java:graphql-java:$graphqlVersion")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:$tomcatEmbedCoreVersion") // overstyrer sårbar dependency
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
