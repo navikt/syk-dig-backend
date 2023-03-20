ff773e99e52a7fad36697087ef46b1235f5dc61c
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-03-20 14:48:54 +0100

    Added in secureLogs

--- src/main/kotlin/no/nav/sykdig/Log.kt
+++ src/main/kotlin/no/nav/sykdig/Log.kt
@@ -6,3 +6,7 @@ import org.slf4j.LoggerFactory
 inline fun <reified T> T.logger(): Logger {
     return LoggerFactory.getLogger(T::class.java)
 }
+
+inline fun <reified T> T.securelog(): Logger {
+    return LoggerFactory.getLogger("securelog")
+}
