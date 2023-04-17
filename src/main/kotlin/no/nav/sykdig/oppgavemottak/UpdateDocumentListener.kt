20adfaab7325dcb05711ebdbb01e310a27e24f5a
Andreas Sagen Aspaas <danduras@gmail.com>
2023-04-17 21:26:37 +0200

    remove error handling in dev

--- src/main/kotlin/no/nav/sykdig/oppgavemottak/UpdateDocumentListener.kt
+++ src/main/kotlin/no/nav/sykdig/oppgavemottak/UpdateDocumentListener.kt
@@ -26,36 +26,28 @@ class UpdateDocumentListener(
         groupId = "syk-dig-update-document-consumer",
     )
     fun listen(cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
-        try {
-            val oppgave: DigitaliseringsoppgaveKafka = objectMapper.readValue(cr.value())
-            val oppgaveDokumenter = (oppgave.dokumenter ?: emptyList()).map {
-                DokumentDbModel(
-                    dokumentInfoId = it.dokumentInfoId,
-                    tittel = it.tittel,
-                )
-            }
-            val safDocuments = safDocumentClient.getDokumenter(oppgave.journalpostId).map {
-                DokumentDbModel(
-                    dokumentInfoId = it.dokumentInfoId,
-                    tittel = it.tittel,
-                )
-            }
+        val oppgave: DigitaliseringsoppgaveKafka = objectMapper.readValue(cr.value())
+        val oppgaveDokumenter = (oppgave.dokumenter ?: emptyList()).map {
+            DokumentDbModel(
+                dokumentInfoId = it.dokumentInfoId,
+                tittel = it.tittel,
+            )
+        }
+        val safDocuments = safDocumentClient.getDokumenter(oppgave.journalpostId).map {
+            DokumentDbModel(
+                dokumentInfoId = it.dokumentInfoId,
+                tittel = it.tittel,
+            )
+        }
 
-            if (safDocuments.isEmpty()) {
-                log.info("Fant ikke journalpost for oppgave: ${oppgave.oppgaveId} - journalpost: ${oppgave.journalpostId}")
-                return
-            }
-
-            if (safDocuments.toSet() == oppgaveDokumenter.toSet()) {
-                log.info("Documents are equal, skipping oppgave ${oppgave.oppgaveId}")
-                return
-            }
+        if (safDocuments.isEmpty()) {
+            log.info("Fant ikke journalpost for oppgave: ${oppgave.oppgaveId} - journalpost: ${oppgave.journalpostId}")
+        } else if (safDocuments.toSet() == oppgaveDokumenter.toSet()) {
+            log.info("Documents are equal, skipping oppgave ${oppgave.oppgaveId}")
+        } else {
             log.info("Oppdaterer dokumenter for oppgave ${oppgave.oppgaveId} fra ${oppgave.source}, antall dokumenter ${safDocuments.size}")
             oppgaveRepository.updateOppgaveDokumenter(oppgave.oppgaveId, safDocuments)
-        } catch (e: Exception) {
-            log.error("error prosessing record ${cr.key()}")
-        } finally {
-            acknowledgment.acknowledge()
         }
+        acknowledgment.acknowledge()
     }
 }
