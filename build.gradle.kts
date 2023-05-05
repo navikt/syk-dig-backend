e76420c4fcb090e5b84ab7815b484ccdf76e6cca
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-05-05 07:45:37 +0200

    downgrade som deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import org.jlleitschuh.gradle.ktlint.KtlintExtension
 plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.spring") version "1.8.21"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.spring") version "1.8.20"
     id("com.netflix.dgs.codegen") version "5.1.17"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
 }
