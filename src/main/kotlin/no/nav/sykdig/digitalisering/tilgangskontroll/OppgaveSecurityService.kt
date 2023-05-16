370d85847ea38a5e506fc92b0ac5383cd61e5ed9
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-05-16 07:47:40 +0200

    Removed tmp securelog

--- src/main/kotlin/no/nav/sykdig/digitalisering/tilgangskontroll/OppgaveSecurityService.kt
+++ src/main/kotlin/no/nav/sykdig/digitalisering/tilgangskontroll/OppgaveSecurityService.kt
@@ -4,7 +4,6 @@ import no.nav.sykdig.auditLogger.AuditLogger
 import no.nav.sykdig.auditlog
 import no.nav.sykdig.digitalisering.SykDigOppgaveService
 import no.nav.sykdig.logger
-import no.nav.sykdig.securelog
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 import org.springframework.stereotype.Service
@@ -18,7 +17,6 @@ class OppgaveSecurityService(
     companion object {
         private val log = logger()
         private val auditlog = auditlog()
-        private val securelog = securelog()
     }
     fun hasAccessToOppgave(oppgaveId: String): Boolean {
         val oppgave = sykDigOppgaveService.getOppgave(oppgaveId)
@@ -26,7 +24,6 @@ class OppgaveSecurityService(
             log.warn("Innlogget bruker har ikke tilgang til oppgave med id $oppgaveId")
             val autentication = SecurityContextHolder.getContext().authentication as JwtAuthenticationToken
             val navEmail = autentication.token.claims["preferred_username"].toString()
-            securelog.info("navEmail: $navEmail")
 
             auditlog.info(
                 AuditLogger().createcCefMessage(
@@ -44,7 +41,6 @@ class OppgaveSecurityService(
 
         val autentication = SecurityContextHolder.getContext().authentication as JwtAuthenticationToken
         val navEmail = autentication.token.claims["preferred_username"].toString()
-        securelog.info("navEmail: $navEmail")
 
         auditlog.info(
             AuditLogger().createcCefMessage(
