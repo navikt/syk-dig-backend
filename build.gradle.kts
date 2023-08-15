97375e3ec5846b3f8978dac2c136bf04af14bf01
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-08-15 07:48:53 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,13 +92,6 @@ dependencies {
 }
 
 tasks {
-    withType<KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "17"
-        }
-    }
-
     withType<Test> {
         useJUnitPlatform()
         testLogging {
