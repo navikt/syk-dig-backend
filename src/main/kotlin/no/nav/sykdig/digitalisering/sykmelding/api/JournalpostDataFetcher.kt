d41cbfea0de719183191c96c7c73563bac4818b9
Andreas Sagen Aspaas <danduras@gmail.com>
2023-11-09 15:13:25 +0100

    Change return types and do postAuthorize
    Co-authored-by: Karl O. <k@rl.run>
    Co-authored-by: Natalie <natalie.uranes@gmail.com>

--- src/main/kotlin/no/nav/sykdig/digitalisering/sykmelding/api/JournalpostDataFetcher.kt
+++ src/main/kotlin/no/nav/sykdig/digitalisering/sykmelding/api/JournalpostDataFetcher.kt
@@ -4,47 +4,65 @@ import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsMutation
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
+import no.nav.sykdig.digitalisering.dokarkiv.BrukerIdType
+import no.nav.sykdig.digitalisering.pdl.PersonService
 import no.nav.sykdig.digitalisering.saf.SafJournalpostGraphQlClient
 import no.nav.sykdig.generated.DgsConstants
 import no.nav.sykdig.generated.types.Document
 import no.nav.sykdig.generated.types.Journalpost
+import no.nav.sykdig.generated.types.JournalpostResult
+import no.nav.sykdig.generated.types.JournalpostStatus
+import no.nav.sykdig.generated.types.JournalpostStatusEnum
+import org.springframework.security.access.prepost.PostAuthorize
 import org.springframework.security.access.prepost.PreAuthorize
 
 @DgsComponent
 class JournalpostDataFetcher(
     private val safGraphQlClient: SafJournalpostGraphQlClient,
+    private val personService: PersonService,
 ) {
 
-    @PreAuthorize("@oppgaveSecurityService.hasAccessToJournalpost(#id)")
+    @PostAuthorize("@oppgaveSecurityService.hasAccessToJournalpost(returnObject)")
     @DgsQuery(field = DgsConstants.QUERY.Journalpost)
     fun getJournalpostById(
         @InputArgument id: String,
-    ): Journalpost {
+    ): JournalpostResult {
         val journalpost = safGraphQlClient.getJournalpost(id)
+        val fnrEllerAktorId = when (journalpost.journalpost?.bruker?.type) {
+            BrukerIdType.ORGNR -> null
+            else -> journalpost.journalpost?.bruker?.id
+        }
+
+        if (fnrEllerAktorId == null) {
+            return JournalpostStatus(
+                journalpostId = id,
+                status = JournalpostStatusEnum.MANGLER_FNR,
+            )
+        }
+
+        val fnr = personService.hentPerson(fnrEllerAktorId, id).fnr
         return Journalpost(
             id,
             journalpost.journalpost?.journalstatus?.name ?: "MANGLER_STATUS",
             dokumenter = journalpost.journalpost?.dokumenter?.map {
                 Document(it.tittel ?: "Mangler Tittel", it.dokumentInfoId)
             } ?: emptyList(),
+            fnr = fnr,
         )
     }
 
-    @PreAuthorize("@oppgaveSecurityService.hasAccessToJournalpost(#journalpostId)")
+    @PreAuthorize("@oppgaveSecurityService.hasAccessToJournalpostId(#journalpostId)")
     @DgsMutation(field = DgsConstants.MUTATION.SykmeldingFraJournalpost)
     fun createSykmelding(
         @InputArgument journalpostId: String,
-    ): Journalpost {
+    ): JournalpostResult {
         val journalpost = safGraphQlClient.getJournalpost(journalpostId)
 
-        // TODO: Opprett sykmelding på Kafka
+        // TODO: Valider og opprett sykmelding på Kafka
 
-        return Journalpost(
-            journalpostId,
-            journalpost.journalpost?.journalstatus?.name ?: "MANGLER_STATUS",
-            dokumenter = journalpost.journalpost?.dokumenter?.map {
-                Document(it.tittel ?: "Mangler Tittel", it.dokumentInfoId)
-            } ?: emptyList(),
+        return JournalpostStatus(
+            journalpostId = journalpostId,
+            status = JournalpostStatusEnum.OPPRETTET,
         )
     }
 }
