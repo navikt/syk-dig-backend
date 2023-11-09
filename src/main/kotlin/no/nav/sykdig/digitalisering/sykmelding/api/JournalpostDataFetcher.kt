95f2876e48444f824cefa61fa317f394539cbde7
Jørn-Are Flaten <ja.flaten91@gmail.com>
2023-11-09 13:39:13 +0100

    create sykmelding api

--- src/main/kotlin/no/nav/sykdig/digitalisering/sykmelding/api/JournalpostDataFetcher.kt
+++ src/main/kotlin/no/nav/sykdig/digitalisering/sykmelding/api/JournalpostDataFetcher.kt
@@ -1,29 +1,46 @@
 package no.nav.sykdig.digitalisering.sykmelding.api
 
 import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsMutation
 import com.netflix.graphql.dgs.DgsQuery
 import com.netflix.graphql.dgs.InputArgument
-import graphql.schema.DataFetchingEnvironment
 import no.nav.sykdig.digitalisering.saf.SafJournalpostGraphQlClient
 import no.nav.sykdig.generated.DgsConstants
 import no.nav.sykdig.generated.types.Document
 import no.nav.sykdig.generated.types.Journalpost
-import org.springframework.security.access.prepost.PostAuthorize
+import org.springframework.security.access.prepost.PreAuthorize
 
 @DgsComponent
 class JournalpostDataFetcher(
     private val safGraphQlClient: SafJournalpostGraphQlClient,
 ) {
 
-    @PostAuthorize("@oppgaveSecurityService.hasAccessToJournalpost(#id)")
+    @PreAuthorize("@oppgaveSecurityService.hasAccessToJournalpost(#id)")
     @DgsQuery(field = DgsConstants.QUERY.Journalpost)
     fun getJournalpostById(
         @InputArgument id: String,
-        dfe: DataFetchingEnvironment,
     ): Journalpost {
         val journalpost = safGraphQlClient.getJournalpost(id)
         return Journalpost(
             id,
+            journalpost.journalpost?.journalstatus?.name ?: "MANGLER_STATUS",
+            dokumenter = journalpost.journalpost?.dokumenter?.map {
+                Document(it.tittel ?: "Mangler Tittel", it.dokumentInfoId)
+            } ?: emptyList(),
+        )
+    }
+
+    @PreAuthorize("@oppgaveSecurityService.hasAccessToJournalpost(#journalpostId)")
+    @DgsMutation(field = DgsConstants.MUTATION.SykmeldingFraJournalpost)
+    fun createSykmelding(
+        @InputArgument journalpostId: String,
+    ): Journalpost {
+        val journalpost = safGraphQlClient.getJournalpost(journalpostId)
+
+        // TODO: Opprett sykmelding på Kafka
+
+        return Journalpost(
+            journalpostId,
             journalpost.journalpost?.journalstatus?.name ?: "MANGLER_STATUS",
             dokumenter = journalpost.journalpost?.dokumenter?.map {
                 Document(it.tittel ?: "Mangler Tittel", it.dokumentInfoId)
