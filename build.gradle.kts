ad398abdce6a37353e1232af9f9bfa18fe4888f3
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-08-09 12:00:15 +0200

    Rolling back spring upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ import org.jlleitschuh.gradle.ktlint.KtlintExtension
 plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
-    kotlin("jvm") version "2.0.10"
-    kotlin("plugin.spring") version "2.0.10"
+    kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "2.0.0"
     id("com.netflix.dgs.codegen") version "5.12.4"
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
     id("org.cyclonedx.bom") version "1.8.2"
