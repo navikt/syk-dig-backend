6281cbe8b9f7c3fe27c807ff4a128779955f4f3d
Helene Arnesen <helene.arnesen@nav.no>
2024-09-17 13:21:09 +0200

    chore: log clientConfigurationProperties

--- src/main/kotlin/no/nav/sykdig/config/M2MTokenService.kt
+++ src/main/kotlin/no/nav/sykdig/config/M2MTokenService.kt
@@ -1,6 +1,7 @@
 package no.nav.sykdig.config
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
+import no.nav.sykdig.applog
 import org.springframework.stereotype.Service
 
 @Service
@@ -8,7 +9,12 @@ class M2MTokenService(
     private val oAuth2AccessTokenService: OAuth2AccessTokenService,
     private val clientConfigurationProperties: ClientConfigurationProperties,
 ) {
+    val log = applog()
     fun getOppgaveM2MToken(): String {
+        clientConfigurationProperties.registration.forEach { (key, value) ->
+            log.info("Client registration found: $key")
+        }
+
         val clientProperties =
             clientConfigurationProperties.registration["oppgave-m2m"]
                 ?: throw RuntimeException("Client properties for 'oppgave-m2m' not found")
