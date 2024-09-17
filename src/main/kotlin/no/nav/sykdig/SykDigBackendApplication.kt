7a3d9d9185b9c4d92a7d2c9368ed84605acb7dac
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-09-17 14:02:20 +0200

    Added EnableConfigurationProperties annotation

--- src/main/kotlin/no/nav/sykdig/SykDigBackendApplication.kt
+++ src/main/kotlin/no/nav/sykdig/SykDigBackendApplication.kt
@@ -1,6 +1,8 @@
 package no.nav.sykdig
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.scheduling.annotation.EnableScheduling
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @SpringBootApplication
 @EnableScheduling
 @EnableKafka
+@EnableConfigurationProperties(OAuth2ClientProperties::class)
 class SykDigBackendApplication
 
 fun main(args: Array<String>) {
