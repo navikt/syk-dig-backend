1fe656e26338cba76a978c3fe770a5ebadba0747
Jørn-Are Flaten <ja.flaten91@gmail.com>
2024-09-20 07:54:51 +0200

    chore: inject properties class

--- src/main/kotlin/no/nav/sykdig/config/M2MTokenService.kt
+++ src/main/kotlin/no/nav/sykdig/config/M2MTokenService.kt
@@ -3,7 +3,15 @@ import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.sykdig.applog
 import no.nav.sykdig.securelog
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Service
+
+@Configuration
+@EnableConfigurationProperties(ClientConfigurationProperties::class)
+class ClientPropertiesConfig {
+
+}
 
 @Service
 class M2MTokenService(
