e1ba09600b362abda8c3a6c4719d4b01f106b772
Helene Arnesen <helene.arnesen@nav.no>
2024-10-08 13:21:38 +0200

    chore: add get manuell oppgave proxy
    Co-authored-by: Natalie Uranes <natalie.uranes@gmail.com>

--- src/main/kotlin/no/nav/sykdig/digitalisering/papirsykmelding/api/SmregistreringClient.kt
+++ src/main/kotlin/no/nav/sykdig/digitalisering/papirsykmelding/api/SmregistreringClient.kt
@@ -8,11 +8,14 @@ import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.HttpServerErrorException
 import org.springframework.web.client.RestTemplate
+import java.time.LocalDate
+import java.time.OffsetDateTime
 
 @Component
 class SmregistreringClient(
@@ -26,17 +29,59 @@ class SmregistreringClient(
     fun postSmregistreringRequest(
         token: String,
         oppgaveId: String,
-    ) {
+        typeRequest: String,
+    ): String {
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         headers.setBearerAuth(token)
-        try {
-            smregisteringRestTemplate.exchange(
-                "$url/oppgave/$oppgaveId/avvis",
-                HttpMethod.POST,
-                HttpEntity(null, headers),
-                String::class.java,
+        return try {
+            val response =
+                smregisteringRestTemplate.exchange(
+                    "$url/oppgave/$oppgaveId/$typeRequest",
+                    HttpMethod.POST,
+                    HttpEntity(null, headers),
+                    String::class.java,
+                )
+            response.body ?: "ingen respons fra server"
+        } catch (e: HttpClientErrorException) {
+            if (e.statusCode.value() == 401 || e.statusCode.value() == 403) {
+                log.warn("smregistering_backend $oppgaveId: ${e.message}")
+                throw IkkeTilgangException("Veileder har ikke tilgang til oppgave")
+            } else {
+                log.error(
+                    "HttpClientErrorException for oppgaveId $oppgaveId med responskode " +
+                        "${e.statusCode.value()} fra Oppgave ved ferdigstilling: ${e.message}",
+                    e,
+                )
+                throw e
+            }
+        } catch (e: HttpServerErrorException) {
+            log.error(
+                "HttpServerErrorException for oppgaveId $oppgaveId med responskode " +
+                    "${e.statusCode.value()} fra Oppgave ved ferdigstilling: ${e.message}",
+                e,
             )
+            throw e
+        }
+    }
+
+    @Retryable
+    fun getSmregistreringRequest(
+        token: String,
+        oppgaveId: String,
+    ): ResponseEntity<PapirManuellOppgave> {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        headers.setBearerAuth(token)
+        return try {
+            val response =
+                smregisteringRestTemplate.exchange(
+                    "$url/oppgave/$oppgaveId",
+                    HttpMethod.GET,
+                    HttpEntity(null, headers),
+                    PapirManuellOppgave::class.java,
+                )
+            response
         } catch (e: HttpClientErrorException) {
             if (e.statusCode.value() == 401 || e.statusCode.value() == 403) {
                 log.warn("smregistering_backend $oppgaveId: ${e.message}")
@@ -59,3 +104,42 @@ class SmregistreringClient(
         }
     }
 }
+
+data class PapirManuellOppgave(
+    val fnr: String?,
+    val sykmeldingId: String,
+    val oppgaveid: Int,
+    var pdfPapirSykmelding: ByteArray,
+    val papirSmRegistering: PapirSmRegistering?,
+    val documents: List<Document>,
+)
+
+data class Document(
+    val dokumentInfoId: String,
+    val tittel: String,
+)
+
+data class PapirSmRegistering(
+    val journalpostId: String,
+    val oppgaveId: String?,
+    val fnr: String?,
+    val aktorId: String?,
+    val dokumentInfoId: String?,
+    val datoOpprettet: OffsetDateTime?,
+    val sykmeldingId: String,
+    val syketilfelleStartDato: LocalDate?,
+    val arbeidsgiver: Arbeidsgiver?,
+    val medisinskVurdering: MedisinskVurdering?,
+    val skjermesForPasient: Boolean?,
+    val perioder: List<Periode>?,
+    val prognose: Prognose?,
+    val utdypendeOpplysninger: Map<String, Map<String, SporsmalSvar>>?,
+    val tiltakNAV: String?,
+    val tiltakArbeidsplassen: String?,
+    val andreTiltak: String?,
+    val meldingTilNAV: MeldingTilNAV?,
+    val meldingTilArbeidsgiver: String?,
+    val kontaktMedPasient: KontaktMedPasient?,
+    val behandletTidspunkt: LocalDate?,
+    val behandler: Behandler?,
+)
