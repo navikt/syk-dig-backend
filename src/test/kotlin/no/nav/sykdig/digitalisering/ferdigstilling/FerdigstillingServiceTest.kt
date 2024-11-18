f33f2dcdda7e5f1bb7bf1a48c90fcc259942e5f7
Helene Arnesen <helene.arnesen@nav.no>
2024-11-18 12:38:00 +0100

    fix: tests

--- src/test/kotlin/no/nav/sykdig/digitalisering/ferdigstilling/FerdigstillingServiceTest.kt
+++ src/test/kotlin/no/nav/sykdig/digitalisering/ferdigstilling/FerdigstillingServiceTest.kt
@@ -72,7 +72,7 @@ class FerdigstillingServiceTest : IntegrationTest() {
     @BeforeEach
     fun setup() {
         ferdigstillingService =
-            FerdigstillingService(safJournalpostGraphQlClient, dokarkivClient, oppgaveClient, sykmeldingOKProducer, dokumentService)
+            FerdigstillingService(safJournalpostGraphQlClient, dokarkivClient, oppgaveClient, sykmeldingOKProducer, dokumentService, safJournalpostGraphQlClient)
     }
 
     @Test
