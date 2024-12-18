0d702b6c2fb64c9873dfe4acdb5c3a075ee570e2
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-12-18 15:40:57 +0100

    Updating JVM version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask
 plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
-    kotlin("jvm") version "2.0.10"
-    kotlin("plugin.spring") version "2.0.10"
+    kotlin("jvm") version "2.0.21"
+    kotlin("plugin.spring") version "2.0.21"
     id("com.netflix.dgs.codegen") version "5.12.4"
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
