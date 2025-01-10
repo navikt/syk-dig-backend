abd7835d2e204e990037ad7c4f5d80da52cc78b4
Hein Haraldsen <hein.haraldsen@bekk.no>
2025-01-10 11:21:07 +0100

    Added back restTemplate for smregisterClient

--- src/main/kotlin/no/nav/sykdig/shared/config/AadRestTemplateConfiguration.kt
+++ src/main/kotlin/no/nav/sykdig/shared/config/AadRestTemplateConfiguration.kt
@@ -5,9 +5,14 @@ import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.EnableOAuth2Client
 import no.nav.sykdig.shared.applog
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpRequest
+import org.springframework.http.client.ClientHttpRequestExecution
+import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.web.client.RestTemplate
 import org.springframework.web.reactive.function.client.ClientRequest
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction
 import org.springframework.web.reactive.function.client.WebClient
@@ -22,73 +27,76 @@ class AadWebClientConfiguration {
     @Bean
     fun istilgangskontrollWebClient(
         clientConfigurationProperties: ClientConfigurationProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
     ): WebClient =
         downstreamWebClient(
             registrationName = "onbehalfof-istilgangskontroll",
             clientConfigurationProperties = clientConfigurationProperties,
-            oAuth2AccessTokenService = oAuth2AccessTokenService
+            oAuth2AccessTokenService = oAuth2AccessTokenService,
         )
 
     @Bean
     fun safWebClient(
         clientConfigurationProperties: ClientConfigurationProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
     ): WebClient =
         downstreamWebClient(
             registrationName = "onbehalfof-saf",
             clientConfigurationProperties = clientConfigurationProperties,
-            oAuth2AccessTokenService = oAuth2AccessTokenService
+            oAuth2AccessTokenService = oAuth2AccessTokenService,
         )
 
     @Bean
     fun pdlWebClient(
         clientConfigurationProperties: ClientConfigurationProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
     ): WebClient =
         downstreamWebClient(
             registrationName = "onbehalfof-pdl",
             clientConfigurationProperties = clientConfigurationProperties,
-            oAuth2AccessTokenService = oAuth2AccessTokenService
+            oAuth2AccessTokenService = oAuth2AccessTokenService,
         )
 
     @Bean
     fun dokarkivWebClient(
         clientConfigurationProperties: ClientConfigurationProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
     ): WebClient =
         downstreamWebClient(
             registrationName = "onbehalfof-dokarkiv",
             clientConfigurationProperties = clientConfigurationProperties,
-            oAuth2AccessTokenService = oAuth2AccessTokenService
+            oAuth2AccessTokenService = oAuth2AccessTokenService,
         )
 
     @Bean
     fun oppgaveWebClient(
         clientConfigurationProperties: ClientConfigurationProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
     ): WebClient =
         downstreamWebClient(
             registrationName = "onbehalfof-oppgave",
             clientConfigurationProperties = clientConfigurationProperties,
-            oAuth2AccessTokenService = oAuth2AccessTokenService
+            oAuth2AccessTokenService = oAuth2AccessTokenService,
         )
 
     @Bean
-    fun smregisteringWebClient(
+    fun smregisteringRestTemplate(
+        restTemplateBuilder: RestTemplateBuilder,
         clientConfigurationProperties: ClientConfigurationProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
-    ): WebClient =
-        downstreamWebClient(
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
+    ): RestTemplate {
+        return downstreamRestTemplate(
             registrationName = "onbehalfof-smreg",
+            restTemplateBuilder = restTemplateBuilder,
             clientConfigurationProperties = clientConfigurationProperties,
-            oAuth2AccessTokenService = oAuth2AccessTokenService
+            oAuth2AccessTokenService = oAuth2AccessTokenService,
         )
+    }
 
     private fun downstreamWebClient(
         registrationName: String,
         clientConfigurationProperties: ClientConfigurationProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
     ): WebClient {
         val clientProperties =
             clientConfigurationProperties.registration[registrationName]
@@ -100,9 +108,34 @@ class AadWebClientConfiguration {
             .build()
     }
 
+    private fun downstreamRestTemplate(
+        restTemplateBuilder: RestTemplateBuilder,
+        clientConfigurationProperties: ClientConfigurationProperties,
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
+        registrationName: String,
+    ): RestTemplate {
+        val clientProperties =
+            clientConfigurationProperties.registration[registrationName]
+                ?: throw RuntimeException("Fant ikke config for $registrationName")
+        return restTemplateBuilder
+            .additionalInterceptors(bearerTokenInterceptor(clientProperties, oAuth2AccessTokenService))
+            .build()
+    }
+
+    private fun bearerTokenInterceptor(
+        clientProperties: ClientProperties,
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
+    ): ClientHttpRequestInterceptor {
+        return ClientHttpRequestInterceptor { request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution ->
+            val response = oAuth2AccessTokenService.getAccessToken(clientProperties)
+            request.headers.setBearerAuth(response.accessToken!!)
+            execution.execute(request, body)
+        }
+    }
+
     private fun bearerTokenFilter(
         clientProperties: ClientProperties,
-        oAuth2AccessTokenService: OAuth2AccessTokenService
+        oAuth2AccessTokenService: OAuth2AccessTokenService,
     ): ExchangeFilterFunction {
         return ExchangeFilterFunction.ofRequestProcessor { clientRequest ->
             val token = oAuth2AccessTokenService.getAccessToken(clientProperties).accessToken
