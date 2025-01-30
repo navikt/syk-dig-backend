359579dcda6444d655b3de8d2b231e171dea8b09
Andreas Sagen Aspaas <danduras@gmail.com>
2025-01-30 13:54:37 +0100

    chore: remove old rule names

--- src/main/kotlin/no/nav/sykdig/nasjonal/models/WhitelistedRuleHit.kt
+++ src/main/kotlin/no/nav/sykdig/nasjonal/models/WhitelistedRuleHit.kt
@@ -2,9 +2,7 @@ package no.nav.sykdig.nasjonal.models
 
 enum class WhitelistedRuleHit {
     INNTIL_8_DAGER,
-    INNTIL_1_MAANED, //fjernes
     MINDRE_ENN_1_MAANED,
-    INNTIL_1_MAANED_MED_BEGRUNNELSE, //fjernes
     MINDRE_ENN_1_MAANED_MED_BEGRUNNELSE,
     OVER_1_MAANED,
     FREMDATERT,
