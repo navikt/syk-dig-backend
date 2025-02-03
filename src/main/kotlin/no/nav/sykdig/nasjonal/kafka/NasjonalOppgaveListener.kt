b1236cd7f4116cf2918dae3c3c98501fbb8f5aa7
Helene Arnesen <helene.arnesen@nav.no>
2025-02-03 12:28:54 +0100

    chore: read topic from start

--- src/main/kotlin/no/nav/sykdig/nasjonal/kafka/NasjonalOppgaveListener.kt
+++ src/main/kotlin/no/nav/sykdig/nasjonal/kafka/NasjonalOppgaveListener.kt
@@ -26,7 +26,7 @@ class NasjonalOppgaveListener(
 
     @KafkaListener(
         topics = ["\${smreg.topic}"],
-        groupId = "papir-sm-registering-consumer-7",
+        groupId = "papir-sm-registering-consumer-9",
         properties = ["auto.offset.reset = earliest"],
         containerFactory = "aivenKafkaListenerContainerFactory",
     )
