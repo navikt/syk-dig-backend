d4ecb6d2d29193dbbfd392b44ccc0a4453b5f269
Helene Arnesen <helene.arnesen@nav.no>
2025-02-12 15:40:17 +0100

    chore: read from migration topic and save in db

---
+++ src/main/kotlin/no/nav/sykdig/nasjonal/kafka/NasjonalOppgaveMigrationListener.kt
@@ -0,0 +1,35 @@
+package no.nav.sykdig.nasjonal.kafka
+
+import com.fasterxml.jackson.module.kotlin.readValue
+import no.nav.sykdig.nasjonal.clients.MigrationObject
+import no.nav.sykdig.nasjonal.services.NasjonalOppgaveService
+import no.nav.sykdig.shared.applog
+import no.nav.sykdig.shared.objectMapper
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.support.Acknowledgment
+import org.springframework.stereotype.Component
+
+
+@Component
+class NasjonalOppgaveMigrationListener(
+    val nasjonalOppgaveService: NasjonalOppgaveService,
+) {
+    val logger = applog()
+
+    @KafkaListener(
+        topics = ["\${smregmigration.topic}"],
+        groupId = "syk-dig-migration-consumer",
+        properties = ["auto.offset.reset = none"],
+        containerFactory = "aivenKafkaListenerContainerFactory",
+    )
+    fun listen(
+        cr: ConsumerRecord<String, String>,
+        acknowledgment: Acknowledgment,
+    ) {
+        val oppgaveRecord: MigrationObject = objectMapper.readValue(cr.value())
+       nasjonalOppgaveService.lagreISykDig(oppgaveRecord)
+
+        acknowledgment.acknowledge()
+    }
+}
