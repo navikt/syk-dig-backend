843b04e049b68348f63852ef180bbb0c9682c991
Natalie Uranes <natalie.uranes@gmail.com>
2025-03-31 12:18:24 +0200

    chore: remove rest /send and /korriger (#437)
    remove rest /send and /korriger for nasjonal sykmelding

--- src/main/kotlin/no/nav/sykdig/nasjonal/api/NasjonalOppgaveController.kt
+++ src/main/kotlin/no/nav/sykdig/nasjonal/api/NasjonalOppgaveController.kt
@@ -1,20 +1,16 @@
 package no.nav.sykdig.nasjonal.api
 
 import io.opentelemetry.instrumentation.annotations.WithSpan
-import no.nav.sykdig.digitalisering.papirsykmelding.mapFromDao
 import no.nav.sykdig.shared.applog
 import no.nav.sykdig.nasjonal.helsenett.SykmelderService
 import no.nav.sykdig.nasjonal.services.NasjonalOppgaveService
-import no.nav.sykdig.nasjonal.models.SmRegistreringManuell
 import no.nav.sykdig.nasjonal.models.Sykmelder
 import no.nav.sykdig.nasjonal.services.NasjonalDbService
 import no.nav.sykdig.pdl.Navn
 import no.nav.sykdig.pdl.PersonService
 import no.nav.sykdig.shared.securelog
-import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
-import org.springframework.security.access.prepost.PostAuthorize
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -25,7 +21,6 @@ class NasjonalOppgaveController(
     private val nasjonalOppgaveService: NasjonalOppgaveService,
     private val sykmelderService: SykmelderService,
     private val personService: PersonService,
-    private val nasjonalDbService: NasjonalDbService
 ) {
     val log = applog()
     val securelog = securelog()
@@ -40,24 +35,6 @@ class NasjonalOppgaveController(
     ): ResponseEntity<HttpStatusCode> {
         log.info("Forsøker å avvise oppgave med oppgaveId: $oppgaveId")
         return nasjonalOppgaveService.avvisOppgave(oppgaveId, avvisSykmeldingRequest, navEnhet)
-    }
-
-    @GetMapping("/oppgave/{oppgaveId}")
-    @PostAuthorize("@oppgaveSecurityService.hasAccessToNasjonalOppgave(#oppgaveId, '/oppgave/{oppgaveId}')")
-    @ResponseBody
-    @WithSpan
-    fun getPapirsykmeldingManuellOppgave(
-        @PathVariable oppgaveId: String,
-    ): ResponseEntity<Any> {
-        val papirManuellOppgave = nasjonalDbService.getOppgaveByOppgaveId(oppgaveId)
-        if (papirManuellOppgave != null) {
-            if(papirManuellOppgave.ferdigstilt) {
-                log.info("Oppgave med id $oppgaveId er allerede ferdigstilt")
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Oppgave med id $oppgaveId er allerede ferdigstilt")
-            }
-            return ResponseEntity.ok(mapFromDao(papirManuellOppgave))
-        }
-        return ResponseEntity.notFound().build()
     }
 
     @GetMapping("/pasient")
@@ -91,19 +68,6 @@ class NasjonalOppgaveController(
         return ResponseEntity.ok(sykmelder)
     }
 
-    @PostMapping("/oppgave/{oppgaveId}/send")
-    @PreAuthorize("@oppgaveSecurityService.hasAccessToNasjonalOppgave(#oppgaveId, '/oppgave/{oppgaveId}/send')")
-    @ResponseBody
-    @WithSpan
-    suspend fun sendOppgave(
-        @PathVariable oppgaveId: String,
-        @RequestHeader("X-Nav-Enhet") navEnhet: String,
-        @RequestBody papirSykmelding: SmRegistreringManuell,
-    ): ResponseEntity<Any> {
-        val callId = UUID.randomUUID().toString()
-        return nasjonalOppgaveService.sendNasjonalOppgave(papirSykmelding, navEnhet, callId, oppgaveId)
-    }
-
     @PostMapping("/oppgave/{oppgaveId}/tilgosys")
     @PreAuthorize("@oppgaveSecurityService.hasAccessToNasjonalOppgave(#oppgaveId, '/oppgave/{oppgaveId}/tilgosys')")
     @WithSpan
@@ -117,18 +81,6 @@ class NasjonalOppgaveController(
         log.info("papirsykmelding: Sender oppgave med id $oppgaveId til Gosys")
         nasjonalOppgaveService.oppgaveTilGosys(oppgaveId)
         return ResponseEntity.noContent().build()
-    }
-
-    @PostMapping("/sykmelding/{sykmeldingId}")
-    @PreAuthorize("@oppgaveSecurityService.hasSuperUserAccessToNasjonalSykmelding(#sykmeldingId, null, '/sykmelding/{sykmeldingId}')")
-    @WithSpan
-    suspend fun korrigerSykmelding(
-        @PathVariable sykmeldingId: String,
-        @RequestHeader("X-Nav-Enhet") navEnhet: String,
-        @RequestBody papirSykmelding: SmRegistreringManuell,
-    ): ResponseEntity<Any> {
-        securelog.info("Oppdaterer korrigert oppgave i syk-dig-backend db $papirSykmelding")
-        return nasjonalOppgaveService.korrigerSykmelding(sykmeldingId, navEnhet, UUID.randomUUID().toString(), papirSykmelding)
     }
 
     @GetMapping("/pdf/{oppgaveId}/{dokumentInfoId}")
