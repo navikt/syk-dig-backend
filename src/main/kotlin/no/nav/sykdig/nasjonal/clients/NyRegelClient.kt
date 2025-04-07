5f25633be795731795ba02f8713d8ebb8fcafadc
Karl O <k@rl.run>
2025-04-07 13:50:02 +0200

    chore: shadow test usage of syfosmregler instead of syfosmpapirregler (#443)

---
+++ src/main/kotlin/no/nav/sykdig/nasjonal/clients/NyRegelClient.kt
@@ -0,0 +1,39 @@
+package no.nav.sykdig.nasjonal.clients
+
+import net.logstash.logback.argument.StructuredArguments.kv
+import no.nav.sykdig.shared.applog
+import no.nav.sykdig.shared.ReceivedSykmelding
+import no.nav.sykdig.shared.securelog
+import no.nav.sykdig.shared.ValidationResult
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.client.RestTemplate
+
+
+@Component
+class NyRegelClient(
+    @Value("\${ny-regel.url}") private val regelUrl: String,
+    private val nyRegelM2mRestTemplate: RestTemplate
+){
+    val log = applog()
+    val securelog = securelog()
+
+    fun valider(sykmelding: ReceivedSykmelding, msgId: String): ValidationResult {
+        log.info("validating against rules {}", kv("sykmeldingId", sykmelding.sykmelding.id))
+        val headers = HttpHeaders()
+        headers["Nav-CallId"] = msgId
+
+        val response = nyRegelM2mRestTemplate.exchange(
+            "$regelUrl/v1/rules/validate/papir",
+            HttpMethod.POST,
+            HttpEntity(sykmelding, headers),
+            ValidationResult::class.java
+        )
+        return response.body ?: throw HttpClientErrorException(HttpStatus.NOT_FOUND, "regelvalidering feilet for sykmeldingId ${sykmelding.sykmelding.id}")
+    }
+}
